cc3f315de79a9ca14b500f69ba56e638b446e92a
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-02 21:27:21 +0300

    task2 completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulElement = document.querySelector("#ingredients");
+ingredients.forEach((el) => {
+  const liElement = document.createElement("li");
+  liElement.textContent = el;
+  liElement.classList.add("item");
+  ulElement.append(liElement);
+});
