6c8523088cbc92bf0e438843c008092c541e8d5a
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-03 21:16:09 +0300

    fixed task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
+//function getRandomHexColor() {
+//  return `#${Math.floor(Math.random() * 16777215)
+//    .toString(16)
+//    .padStart(6, 0)}`;
+//}
+//const buttonCreate = document.querySelector("[data-create]");
+//const buttonDestroy = document.querySelector("[data-destroy]");
+//const quontityDivsInput = document.querySelector("input");
+//const containerElement = document.querySelector("#boxes");
+//let sideSize = 30;
+
+//buttonCreate.addEventListener("click", createBoxes);
+//buttonDestroy.addEventListener("click", destroyBoxes);
+
+//function createBoxes() {
+//  const amount = Number(quontityDivsInput.value);
+//  console.log(amount);
+//  let markup = "";
+
+//  for (let i = 0; i < amount; i += 1) {
+//    let color = getRandomHexColor();
+
+//    markup += `<div  style="background-color:${color}; width:${sideSize}px; height:${sideSize}px"></div>`;
+//    sideSize += 10;
+//  }
+
+//  sideSize = 30;
+//  containerElement.innerHTML = markup;
+//}
+
+//function destroyBoxes() {
+//  containerElement.innerHTML = "";
+//}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
@@ -9,21 +43,19 @@ const quontityDivsInput = document.querySelector("input");
 const containerElement = document.querySelector("#boxes");
 let sideSize = 30;
 
-buttonCreate.addEventListener("click", createBoxes);
+buttonCreate.addEventListener("click", () => {
+  const amount = quontityDivsInput.value;
+  createBoxes(amount);
+});
 buttonDestroy.addEventListener("click", destroyBoxes);
 
-function createBoxes() {
-  const amount = Number(quontityDivsInput.value);
-  console.log(amount);
+function createBoxes(amount) {
   let markup = "";
-
   for (let i = 0; i < amount; i += 1) {
     let color = getRandomHexColor();
-
     markup += `<div  style="background-color:${color}; width:${sideSize}px; height:${sideSize}px"></div>`;
     sideSize += 10;
   }
-
   sideSize = 30;
   containerElement.innerHTML = markup;
 }
