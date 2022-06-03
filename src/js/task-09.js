32254528277b5f7c072c67ba6c0dfa57d17c694e
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-03 13:14:05 +0300

    task 9 completed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector("button.change-color");
+const colorValueSpan = document.querySelector("span.color");
+
+changeColorBtn.addEventListener("click", clickChangeColorBtnHandler);
+
+function clickChangeColorBtnHandler() {
+  const colorValue = getRandomHexColor();
+  colorValueSpan.textContent = colorValue;
+  document.body.style.backgroundColor = colorValue;
+}
