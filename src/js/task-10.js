7b85fe84499bb967d40d2286e72af45a37e03e78
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-03 17:27:47 +0300

    task 10 completed

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const buttonCreate = document.querySelector("[data-create]");
+const buttonDestroy = document.querySelector("[data-destroy]");
+const quontityDivsInput = document.querySelector("input");
+const containerElement = document.querySelector("#boxes");
+let sideSize = 30;
+
+buttonCreate.addEventListener("click", createBoxes);
+buttonDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(quontityDivsInput.value);
+  console.log(amount);
+  let markup = "";
+
+  for (let i = 0; i < amount; i += 1) {
+    let color = getRandomHexColor();
+
+    markup += `<div  style="background-color:${color}; width:${sideSize}px; height:${sideSize}px"></div>`;
+    sideSize += 10;
+  }
+
+  sideSize = 30;
+  containerElement.innerHTML = markup;
+}
+
+function destroyBoxes() {
+  containerElement.innerHTML = "";
 }
