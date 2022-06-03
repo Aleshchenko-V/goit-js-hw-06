7b3e4f92e30cf3661c8924b4897092173d44f072
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-03 12:53:55 +0300

    task8 completed

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", submitButtonSubmitHandler);
+
+function submitButtonSubmitHandler(e) {
+  e.preventDefault();
+
+  const { email, password } = e.currentTarget.elements;
+
+  if (!email.value.length || !password.value.length) {
+    return alert("All fields should be filled!");
+  }
+
+  console.log({ email: email.value, password: password.value });
+
+  e.currentTarget.reset();
+}
